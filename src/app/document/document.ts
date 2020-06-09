0d04646500d1b11166aaa37f20e8399d7746afc6
Mamatha <mamatha.k@al-enterprise.com>
2020-06-09 17:38:01 +0530

    Added

--- src/app/document/document.ts
+++ src/app/document/document.ts
@@ -4,8 +4,7 @@ export class Document{
     link ?: string;
     categoryName ?: any
 
-    constructor(documentId? : string, documentName? : string,
-        link? : string, categoryName ?: any){
+    constructor(documentId? : string, documentName? : string, link? : string, categoryName ?: any){
         this.documentId = documentId;
         this.documentName = documentName;
         this.link = link;
